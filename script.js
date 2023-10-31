ae7077ddb82c676d2f1366442d70278a12383c41
LN27100 <heloceinlove@hotmail.fr>
2023-10-31 16:46:58 +0100

    initialisation site

---
+++ script.js
@@ -0,0 +1,5 @@
+fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=2e5639d78c766e927181ac78f41ae2dc&lang=fr&units=metric`)
+.then(reponse => reponse.json())
+.then(data => {
+    console.log(data)
+})
