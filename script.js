74c418f04eef4a5e8fc218b095416743dc4cfdb1
LN27100 <heloceinlove@hotmail.fr>
2023-10-31 16:48:48 +0100

    ajout ville

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let city='rouen'
+
 fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=2e5639d78c766e927181ac78f41ae2dc&lang=fr&units=metric`)
 .then(reponse => reponse.json())
 .then(data => {
