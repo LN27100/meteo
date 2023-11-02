b6f7f0693941eac2213d225ae77ca23b8d61472e
LN27100 <heloceinlovehotmail.fr>
2023-11-02 16:13:00 +0100

    ajout alerte tempête

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 // Fonction pour mettre à jour la ville affichée
 function updateCity(cityName) {
-    const cityElement = document.getElementById('city');
-    cityElement.textContent = cityName;
+    const cityElement = document.getElementById('city')
+    cityElement.textContent = cityName
 }
 
 // Fonction pour récupérer les données météorologiques pour une ville
@@ -11,43 +11,43 @@ function fetchDataForCity(city) {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=2e5639d78c766e927181ac78f41ae2dc&lang=fr&units=metric`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            console.log(data)
 
             const weatherInfo = document.getElementById('weather-info');
-            weatherInfo.innerHTML = '';
+            weatherInfo.innerHTML = '' // Vide le contenu existant
 
-            let currentDay = null;
-            let dayContainer = null;
+            let currentDay = null // Variable pour stocker la date du jour en cours
+            let dayContainer = null // Variable pour stocker le conteneur HTML du jour en cours
 
             // parcoure chaque élément du tableau prévisionnel et exécute une fonction donnée sur chaque élément de celui-ci.
             data.list.forEach(item => {
-                const date = new Date(item.dt * 1000);
-                const day = date.toLocaleDateString();
-                const weatherIconUrl = `https://openweathermap.org/img/wn/${item.weather[0].icon}.png`; // URL de l'icône récupérée directement depuis l'API
-                const humidity = item.main.humidity;
-                const minTemp = item.main.temp_min;
-                const maxTemp = item.main.temp_max;
+                const date = new Date(item.dt * 1000)
+                const day = date.toLocaleDateString()
+                const weatherIconUrl = `https://openweathermap.org/img/wn/${item.weather[0].icon}.png` // URL de l'icône récupérée directement depuis l'API.Chaîne de caractères qui identifie l'icône météorologique associée à ces conditions.
+                const humidity = item.main.humidity
+                const minTemp = item.main.temp_min
+                const maxTemp = item.main.temp_max
                 const gustSpeedMs = item.wind.gust;
-                const gustSpeedKmh = (gustSpeedMs * 3.6).toFixed(2);
-                const rain = item.rain ? item.rain['3h'] : 0; // Vérifiez si la propriété 'rain' existe
-                const temp = item.main.temp;
-                const description = item.weather[0].description;
-                const windSpeedMs = item.wind.speed;
-                const windSpeedKmh = (windSpeedMs * 3.6).toFixed(2);
+                const gustSpeedKmh = (gustSpeedMs * 3.6).toFixed(2)
+                const rain = item.rain ? item.rain['3h'] : 0 // Vérifiez si la propriété 'rain' existe
+                const temp = item.main.temp
+                const description = item.weather[0].description
+                const windSpeedMs = item.wind.speed
+                const windSpeedKmh = (windSpeedMs * 3.6).toFixed(2)
 
                 // Crée un conteneur de jour si la date change
                 if (day !== currentDay) {
-                    dayContainer = document.createElement('div');
-                    dayContainer.className = 'day-container';
-                    dayContainer.innerHTML = `<h3>${day}</h3>`;
-                    weatherInfo.appendChild(dayContainer);
+                    dayContainer = document.createElement('div')
+                    dayContainer.className = 'day-container'
+                    dayContainer.innerHTML = `<h3>${day}</h3>`
+                    weatherInfo.appendChild(dayContainer)
 
-                    currentDay = day;
+                    currentDay = day // Met à jour la date du jour en cours
                 }
 
-
-                const weatherItem = document.createElement('div');
-                weatherItem.className = 'weather-card';
+                // élément HTML pour affichant les informations météorologiques
+                const weatherItem = document.createElement('div')
+                weatherItem.className = 'weather-card'
                 weatherItem.innerHTML = `
                     <h4 class="tittle">${date.toLocaleString()}</h4>
                     <img src="${weatherIconUrl}" alt="Weather Icon">
@@ -61,27 +61,29 @@ function fetchDataForCity(city) {
                     <p class="weather-info">Pluie (3 heures) : ${rain} mm</p>
                 `;
 
-                dayContainer.appendChild(weatherItem);
-            });
+                dayContainer.appendChild(weatherItem)
+            })
         })
 
 }
 
 // Recherche de la nouvelle ville entrée
-const searchButton = document.getElementById('search-button');
+const searchButton = document.getElementById('search-button')
 searchButton.addEventListener('click', () => {
-    const newCityInput = document.getElementById('new-city');
-    const newCityName = newCityInput.value.trim();
+    const newCityInput = document.getElementById('new-city')
+    const newCityName = newCityInput.value.trim()
 
+    // Vérifie si l'utilisateur a saisi un nom de ville valide
     if (newCityName) {
         // Mettre à jour la ville actuelle
-        updateCity(newCityName);
+        updateCity(newCityName)
 
         // Effectuer une nouvelle recherche pour la ville spécifiée
-        fetchDataForCity(newCityName);
+        fetchDataForCity(newCityName)
     }
-    newCityInput.value = '';
-});
+    newCityInput.value = '' // RVide la zone de rechercher
+
+})
 
 // Recherche de la ville par défaut
-fetchDataForCity('Val-de-Reuil');
+fetchDataForCity('Val-de-Reuil')
